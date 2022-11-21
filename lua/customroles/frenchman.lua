b787d3b06e24628b13a5673481e2d8d48a973383
The Stig <TheStig294@users.noreply.github.com>
2022-11-21 13:31:25 +1030

    Frenchman baguette now one-shot kills by default

--- lua/customroles/frenchman.lua
+++ lua/customroles/frenchman.lua
@@ -137,7 +137,7 @@ if SERVER then
     local frenchman_adrenaline_baguette = CreateConVar("ttt_frenchman_adrenaline_baguette", "1")
     local frenchman_adrenaline_ramble = CreateConVar("ttt_frenchman_adrenaline_ramble", "1")
     local frenchman_hide_when_active = CreateConVar("ttt_frenchman_hide_when_active", "0")
-    local frenchman_adrenaline_baguette_damage = CreateConVar("ttt_frenchman_adrenaline_baguette_damage", "50", FCVAR_NONE, "Damage the baguette deals", 0, 1000)
+    local frenchman_adrenaline_baguette_damage = CreateConVar("ttt_frenchman_adrenaline_baguette_damage", "1000", FCVAR_NONE, "Damage the baguette deals", 0, 1000)
 
     hook.Add("TTTSyncGlobals", "Frenchman_TTTSyncGlobals", function()
         SetGlobalInt("ttt_frenchman_drain_health_to", frenchman_drain_health_to:GetInt())
